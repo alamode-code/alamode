1ca21ce78c94208f2f657a8736795e117659cacc
Terumasa TADANO <terumasa.tadano@gmail.com>
2025-06-04 15:53:05 +0900

    comment out declaration of non-existing func

--- alm/rref.h
+++ alm/rref.h
@@ -15,10 +15,10 @@ void rref_sparse(const size_t ncols,
                  ConstraintSparseForm &sp_constraint,
                  const double tolerance = 1.0e-12);
 
-void rref_sparse2(const size_t ncols,
-                  ConstraintSparseForm &sp_constraint,
-                  const double tolerance = 1.0e-12);
-
-void rref_sparse_fullpivot(const size_t ncols,
-                           ConstraintSparseForm &sp_constraint,
-                           const double tolerance = 1.0e-12);
+// void rref_sparse2(const size_t ncols,
+//                   ConstraintSparseForm &sp_constraint,
+//                   const double tolerance = 1.0e-12);
+//
+// void rref_sparse_fullpivot(const size_t ncols,
+//                            ConstraintSparseForm &sp_constraint,
+//                            const double tolerance = 1.0e-12);
